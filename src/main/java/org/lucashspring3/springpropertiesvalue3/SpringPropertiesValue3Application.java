bf62ab5c0c7da1a6f3136557767fd78067ea3d03
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-08-22 21:21:27 -0300

    initial commit

---
+++ src/main/java/org/lucashspring3/springpropertiesvalue3/SpringPropertiesValue3Application.java
@@ -0,0 +1,13 @@
+package org.lucashspring3.springpropertiesvalue3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPropertiesValue3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPropertiesValue3Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/lucashspring3/springpropertiesvalue3/SpringPropertiesValue3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lucashspring3.springpropertiesvalue3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPropertiesValue3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
