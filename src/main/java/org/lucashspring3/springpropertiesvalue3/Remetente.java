96b3d62d2fb9a9b830f4c99c6a7da868d5e98551
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-08-22 22:22:28 -0300

    .

---
+++ src/main/java/org/lucashspring3/springpropertiesvalue3/Remetente.java
@@ -0,0 +1,40 @@
+package org.lucashspring3.springpropertiesvalue3;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+@Configuration
+@ConfigurationProperties(prefix = "remetente")
+public class Remetente {
+    private String nome;
+    private String email;
+    private List<Long> telefones = new ArrayList<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Long> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(List<Long> telefones) {
+        this.telefones = telefones;
+    }
+}
--- src/main/java/org/lucashspring3/springpropertiesvalue3/SistemaMensagem.java
+++ src/main/java/org/lucashspring3/springpropertiesvalue3/SistemaMensagem.java
@@ -1,24 +1,22 @@
 package org.lucashspring3.springpropertiesvalue3;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Component
 public class SistemaMensagem implements CommandLineRunner {
-    @Value("${nome}")
-    private String nome;
-    @Value("${email}")
-    private String email;
-    @Value("${telefones}")
-    private List<Long> telefones = new ArrayList<>();
+    @Autowired
+    private Remetente remetente;
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Mensagem enviada por: " + nome + "\nEmail: " + email + "\nTelefones: " + telefones);
+        System.out.println("Mensagem enviada por: " + remetente.getNome()
+                + "\nEmail: " + remetente.getEmail()
+                + "\nTelefones: " + remetente.getTelefones());
         System.out.println("Seu cadastro foi aprovado");
 
     }
